956e38ec9ad2d7313e9710af95d32fb383db1fca
Anna Wrobel <wro.online@gmail.com>
2023-02-03 13:19:17 +0100

    VIP

--- src/background.ts
+++ src/background.ts
@@ -21,19 +21,3 @@ chrome.runtime.onMessage.addListener((message, sender, response) => {
   return true;
 });
 
-setTimeout(() => {
-  chrome.notifications.create(
-    'test',
-    {
-      type: 'basic',
-      title: 'notification',
-      message: 'click me',
-      iconUrl: 'https://cdn-icons-png.flaticon.com/512/616/616430.png',
-      eventTime: Date.now() + 1000,
-    }, function (id) {
-      console.log("Last error:", chrome.runtime.lastError);
-    }
-  )
-}, 1000);
-
-console.log('notification created');
--- src/popup.tsx
+++ src/popup.tsx
@@ -7,3 +7,27 @@ const root = document.querySelector<HTMLDivElement>('#container');
 if (root) {
   createRoot(root).render(<App />);
 }
+
+setTimeout(() => {
+  chrome.notifications.create(
+    'test',
+    {
+      type: 'basic',
+      title: 'notification',
+      message: 'click me',
+      iconUrl: 'icons/icon_32.png',
+      eventTime: Date.now() + 1000,
+      buttons: [
+        {
+          title: 'Yes',
+        },
+        {
+          title: 'No',
+        },
+      ],
+    },
+    function (id) {
+      console.log('Last error:', chrome.runtime.lastError);
+    }
+  );
+}, 1000);
